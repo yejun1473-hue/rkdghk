b325cb1c78de8f9d0690b39a68efeb69524a3db9
yejun1473-hue <yejun1473@gmail.com>
2025-12-25 14:22:47 +0900

    이게 뭐지

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Authentication middleware
